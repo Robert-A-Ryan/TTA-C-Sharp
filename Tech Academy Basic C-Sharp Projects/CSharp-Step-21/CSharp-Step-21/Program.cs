0b2e92e50d49cd9b223085d6856312e8f8bd64b7
Robert Ryan <coachrobertryan@gmail.com>
2019-11-18 12:38:50 -0800

    First commit to my C-Sharp basic

---
+++ Tech Academy Basic C-Sharp Projects/CSharp-Step-21/CSharp-Step-21/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CSharp_Step_21
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
---
+++ Tech Academy Basic C-Sharp Projects/WelcomeAAS/WelcomeAAS/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Services");
+        Console.WriteLine("Remember we're \"accounting\" on you!");
+        Console.ReadLine();
+    }
+}
