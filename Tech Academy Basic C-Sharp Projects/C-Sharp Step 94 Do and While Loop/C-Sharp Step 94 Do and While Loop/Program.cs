2dd9f3ed11d5cea942e6c357d594cfcb360683f9
Robert Ryan <coachrobertryan@gmail.com>
2019-11-19 10:31:38 -0800

    Do and While Loop Practice

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 94 Do and While Loop/C-Sharp Step 94 Do and While Loop/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+
+namespace C_Sharp_Step_94_Do_and_While_Loop
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is my favorite color?");
+            string guess = Console.ReadLine();
+            bool myColor = guess == "red";
+
+            //while (!myColor)
+            //{
+            //    switch (guess)
+            //    {
+            //        case "green":
+            //            Console.WriteLine("You guessed green. Try again.");
+            //            Console.WriteLine("Guess my favorite color.");
+            //            guess = Console.ReadLine();
+            //            break;
+            //        case "blue":
+            //            Console.WriteLine("You guessed blue. Try again.");
+            //            Console.WriteLine("Guess my favorite color.");
+            //            guess = Console.ReadLine();
+            //            break;
+            //        case "yellow":
+            //            Console.WriteLine("You guessed yellow. Try again.");
+            //            Console.WriteLine("Guess my favorite color.");
+            //            guess = Console.ReadLine();
+            //            break;
+            //        case "red":
+            //            Console.WriteLine("You guessed red, you are correct!");
+            //            myColor = true;
+            //            break;
+            //        case "orange":
+            //            Console.WriteLine("You guessed orange. Try again.");
+            //            Console.WriteLine("Guess my favorite color.");
+            //            guess = Console.ReadLine();
+            //            break;
+            //        case "purple":
+            //            Console.WriteLine("You guessed purple. Try again.");
+            //            Console.WriteLine("Guess my favorite color.");
+            //            guess = Console.ReadLine();
+            //            break;
+            //        default:
+            //            Console.WriteLine("You are incorrect.");
+            //            Console.WriteLine("Guess my favorite color.");
+            //            guess = Console.ReadLine();
+            //            break;
+            //    }
+            //}
+
+            do
+            {
+                switch (guess)
+                {
+                    case "green":
+                        Console.WriteLine("You guessed green. Try again.");
+                        Console.WriteLine("Guess my favorite color.");
+                        guess = Console.ReadLine();
+                        break;
+                    case "blue":
+                        Console.WriteLine("You guessed blue. Try again.");
+                        Console.WriteLine("Guess my favorite color.");
+                        guess = Console.ReadLine();
+                        break;
+                    case "yellow":
+                        Console.WriteLine("You guessed yellow. Try again.");
+                        Console.WriteLine("Guess my favorite color.");
+                        guess = Console.ReadLine();
+                        break;
+                    case "red":
+                        Console.WriteLine("You guessed red, you are correct!");
+                        myColor = true;
+                        break;
+                    case "orange":
+                        Console.WriteLine("You guessed orange. Try again.");
+                        Console.WriteLine("Guess my favorite color.");
+                        guess = Console.ReadLine();
+                        break;
+                    case "purple":
+                        Console.WriteLine("You guessed purple. Try again.");
+                        Console.WriteLine("Guess my favorite color.");
+                        guess = Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("You are incorrect.");
+                        Console.WriteLine("Guess my favorite color.");
+                        guess = Console.ReadLine();
+                        break;
+                }
+            }
+            while (!myColor);
+
+            Console.ReadLine();
+        }
+    }
+}
