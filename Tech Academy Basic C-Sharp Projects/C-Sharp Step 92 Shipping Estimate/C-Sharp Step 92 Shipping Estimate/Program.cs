cc5fcfc5127343241a312656fcc3c6a6a06a69f1
Robert Ryan <coachrobertryan@gmail.com>
2019-11-21 12:02:00 -0800

    removed the env.exit and replaced with else statemtn

--- Tech Academy Basic C-Sharp Projects/C-Sharp Step 92 Shipping Estimate/C-Sharp Step 92 Shipping Estimate/Program.cs
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 92 Shipping Estimate/C-Sharp Step 92 Shipping Estimate/Program.cs
@@ -6,40 +6,36 @@ namespace C_Sharp_Step_92_Shipping_Estimate
     {
         static void Main()
         {
-            Console.WriteLine("Welcome to Package Express. Please follow the instrutions below.");
-            Console.WriteLine("");
+            Console.WriteLine("Welcome to Package Express. Please follow the instrutions below.\n");
             Console.WriteLine("What is the weight of the package?");
             int pWeight = Convert.ToInt32(Console.ReadLine());
 
             if (pWeight > 50)
             {
                 Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a good day.");
-                Console.ReadLine();
-                Environment.Exit(-1);
-            }
-
-            Console.WriteLine("");
-            Console.WriteLine("What is the package width?");
-            int pWidth = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is the package height?");
-            int pHeight = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What is the package lenght?");
-            int pLenght = Convert.ToInt32(Console.ReadLine());
-            int pSize = pWidth + pHeight + pLenght;
-
-            if (pSize > 50)
-            {
-                Console.WriteLine("Package is too large to be shipped via Package Express. Have a good day.");
-                Console.ReadLine();
-                Environment.Exit(-1);
             }
             else
             {
-                decimal price = (pSize * pWeight) / 100;
-                Console.WriteLine("");
-                Console.WriteLine("Your shipping rate is $" + price + ". Thank you.");
-            }
+                Console.WriteLine("\nWhat is the package width?");
+                int pWidth = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the package height?");
+                int pHeight = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("What is the package lenght?");
+                int pLenght = Convert.ToInt32(Console.ReadLine());
+                int pSize = pWidth + pHeight + pLenght;
 
+                if (pSize > 50)
+                {
+                    Console.WriteLine("Package is too large to be shipped via Package Express. Have a good day.");
+                }
+                else
+                {
+                    decimal price = (pSize * pWeight) / 100;
+                    Console.WriteLine("\nYour shipping rate is $" + price + ". Thank you.");
+                }
+
+            }
+            
 
 
             Console.ReadLine();
