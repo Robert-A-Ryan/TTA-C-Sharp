3baaffda26c4e423fba52d206eccb5ee3b3ceeb9
Robert Ryan <coachrobertryan@gmail.com>
2019-11-21 14:14:51 -0800

    C-Sharp Step 113 Class_Method

--- Tech Academy Basic C-Sharp Projects/C-Sharp Step 113/C-Sharp Step 113/Step113.cs
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 113/C-Sharp Step 113/Step113.cs
@@ -6,8 +6,10 @@ using System.Threading.Tasks;
 
 namespace C_Sharp_Step_113
 {
+    //create the class
     public class Step113
     {
+        //create a void method for the class that takes 2 ints as parameters.  First int will do a math op, the second will be printed to the console.
         public void FunM (int num1, int num2)
         {
             int wonder = num1 * 24;
