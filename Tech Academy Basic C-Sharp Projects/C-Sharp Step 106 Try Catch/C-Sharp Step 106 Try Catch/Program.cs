99880148651c66d48617b5d4cd8d063951a22672
Robert Ryan <coachrobertryan@gmail.com>
2019-11-20 16:05:05 -0800

    C-Sharp Step 106 Error Handling

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 106 Try Catch/C-Sharp Step 106 Try Catch/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_106_Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> lNumbers = new List<decimal>() { 4, 6, 10, 12, 24, 62, 10005 };
+
+            
+
+            try
+            {
+                Console.WriteLine("Please choose a number.");
+                decimal uNumber = Convert.ToDecimal(Console.ReadLine());
+
+                foreach (decimal number in lNumbers)
+                {
+                    decimal dNumber = number / uNumber;
+                    Console.WriteLine(number + " divided by your number " + uNumber + " equals " + dNumber);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Unable to divide by 0.");
+            }
+            catch (System.FormatException ex)
+            {
+                Console.WriteLine("Unable to divide by non number.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
