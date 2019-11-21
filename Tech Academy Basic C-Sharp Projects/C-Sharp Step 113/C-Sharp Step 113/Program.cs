f2c6da8c670d3ecc1b051dc3e58f82d0a5f08538
Robert Ryan <coachrobertryan@gmail.com>
2019-11-21 14:13:46 -0800

    C-Sharp Step 113 Class_Method

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 113/C-Sharp Step 113/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // declare to integer variables and assign values
+            int num1 = 4;
+            int num2 = 6;
+
+            //instantiate an object of our class, and call the class function with two numbers
+            Step113 step = new Step113();
+            step.FunM(4, 6);
+
+            //call the class function passing the named parameters
+            step.FunM(num1, num2);
+
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 113/C-Sharp Step 113/Step113.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_113
+{
+    public class Step113
+    {
+        public void FunM (int num1, int num2)
+        {
+            int wonder = num1 * 24;
+            Console.WriteLine(num2);
+        }
+    }
+}
