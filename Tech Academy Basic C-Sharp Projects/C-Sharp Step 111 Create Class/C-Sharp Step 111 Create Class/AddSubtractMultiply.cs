731b69e5f0fe8e2cf235974dde92f9d6ea5413cb
Robert Ryan <coachrobertryan@gmail.com>
2019-11-21 11:40:12 -0800

    C-Sharp Step 111 Create Class

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 111 Create Class/C-Sharp Step 111 Create Class/AddSubtractMultiply.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_111_Create_Class
+{
+    public class AddSubtractMultiply
+    {
+        public int num1;
+        public int Add(int num1)
+        {
+            return num1 + 150;
+            //Console.WriteLine("150 added to your number = " + num1);
+            //return num1;
+        }
+
+        public int Subtract(int num1)
+        {
+            return num1 - 24;
+        }
+
+        public int Multiply(int num1)
+        {
+            return num1 * 55;
+        }
+    }
+}
---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 111 Create Class/C-Sharp Step 111 Create Class/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_111_Create_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddSubtractMultiply myMath = new AddSubtractMultiply();
+            
+            Console.WriteLine("Please enter a whole number.");            
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine("150 added to your number = " + myMath.Add(num1));
+            Console.WriteLine("");
+            Console.WriteLine("24 subtracted from your number = " + myMath.Subtract(num1));
+            Console.WriteLine("");
+            Console.WriteLine("Your number multiplied by 55 = " + myMath.Multiply(num1));
+
+            Console.ReadLine();
+            
+
+            
+            
+            
+        }
+    }
+}
