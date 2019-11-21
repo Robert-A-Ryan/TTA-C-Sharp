21249fe7d59fc07dde942ffe2cf713926bee1baa
Robert Ryan <coachrobertryan@gmail.com>
2019-11-21 15:21:41 -0800

    C-Sharp Step 113 More Class Method

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 114 More_Class_Method/C-Sharp Step 114 More_Class_Method/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_114_More_Class_Method
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //#1 using an int variable
+            SomeMath SomeMath = new SomeMath();
+
+            int num1 = 5;
+
+            Console.WriteLine(SomeMath.MathSome(num1));
+            
+
+            //#2 using a decimal variable
+            SomeMath MathSome = new SomeMath();
+
+            decimal num2 = 2.26m;
+
+            Console.WriteLine(MathSome.MathSome(num2));
+
+
+            //#3 using a string converted to int
+            SomeMath sm = new SomeMath();
+
+            string num3 = "4";
+
+            try
+            {
+                Console.WriteLine(sm.MathSome(num3));
+            }
+            catch (UriFormatException ex)
+            {
+                Console.WriteLine("Please provide a number.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+
+
+
+            Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 114 More_Class_Method/C-Sharp Step 114 More_Class_Method/SomeMath.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_114_More_Class_Method
+{
+    class SomeMath
+    {
+        public int MathSome (int num1)
+        {
+            num1 = num1 * 50;
+            return num1;
+        }
+
+        public decimal MathSome(decimal num2)
+        {
+            num2 = num2 + 1.56m;
+            return num2;
+        }
+
+        public int MathSome(string num3)
+        {
+            int result = Convert.ToInt32(num3) + 92;
+            return result;
+        }
+    }
+}
