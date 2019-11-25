5b484b896b442ca9e6bb5a872146437f82ce1c5b
Robert Ryan <coachrobertryan@gmail.com>
2019-11-25 14:43:17 -0800

    C-Sharp Step 124 Abstract

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 124 Abstract/C-Sharp Step 124 Abstract/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_124_Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee() { firstName = "Sample", lastName = "Student" };
+            Emp1.SayName();
+
+            Console.ReadLine();
+        }
+    }
+}
