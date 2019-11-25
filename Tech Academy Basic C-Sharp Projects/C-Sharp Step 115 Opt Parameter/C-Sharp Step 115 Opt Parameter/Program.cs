5db18adb4378c313eea59ecf674350d6615c6c3d
Robert Ryan <coachrobertryan@gmail.com>
2019-11-25 10:43:55 -0800

    C-Sharp Step 115 init

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 115 Opt Parameter/C-Sharp Step 115 Opt Parameter/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_115_Opt_Parameter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RecOpt ro = new RecOpt();
+
+            try
+            {
+                int prod1;
+                int num2;
+                Console.WriteLine("Pick a number.");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Pick an optional number, leave blank if wanted.");
+                string sNum2 = (Console.ReadLine());
+                
+                if (sNum2 != "")
+                {
+                    num2 = Convert.ToInt32(sNum2);
+                }
+                else
+                {
+                    num2 = 0;
+                }
+
+
+                prod1 = ro.RAdd(num1, num2);
+                Console.WriteLine("52 + {0} + {1} = " + prod1, num1, num2);
+                Console.ReadLine();
+            }
+            catch (UriFormatException)
+            {
+                Console.WriteLine("Please type a number.");
+                return;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return;
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 115 Opt Parameter/C-Sharp Step 115 Opt Parameter/RecOpt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_115_Opt_Parameter
+{
+    public class RecOpt
+    {
+        public int RAdd(int num1, int num2 = 0)
+        {
+            //int prod1 = 52 + num1 + num2;
+            return 52 + num1 + num2;
+        }
+        
+    }
+}
+
