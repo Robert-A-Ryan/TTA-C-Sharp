e9f036ba17bd162d2df4783709dbeb8c726a2ec6
Robert Ryan <coachrobertryan@gmail.com>
2019-11-25 13:56:02 -0800

    C-Sharp Step 120 init

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 120 Inherit/C-Sharp Step 120 Inherit/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_120_Inherit
+{
+    public class Employee : Person
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 120 Inherit/C-Sharp Step 120 Inherit/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_120_Inherit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee() { FirstName = "Sample", LastName = "Student" };
+
+            Emp1.SayName();
+
+            Console.ReadLine();
+
+        }
+    }
+}
