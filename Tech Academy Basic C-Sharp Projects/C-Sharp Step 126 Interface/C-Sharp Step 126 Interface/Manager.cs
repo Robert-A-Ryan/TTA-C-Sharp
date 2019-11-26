516d24237f9b746056f84b6296640ca6f5c746c2
Robert Ryan <coachrobertryan@gmail.com>
2019-11-25 16:04:39 -0800

    C-Sharp Step 126 init

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 126 Interface/C-Sharp Step 126 Interface/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_126_Interface
+{
+    public class Manager : Person, IQuittable
+    {
+        public void Quit()
+        {
+            Console.WriteLine("{0} {1} ended thier employment.", firstName, lastName);
+        }
+    }
+}
---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 126 Interface/C-Sharp Step 126 Interface/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_126_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee Emp1 = new Employee() { firstName = "Sample", lastName = "Student" };
+            Emp1.SayName();
+            Emp1.Quit();
+
+            //################################ is this what #3 is referring to?  Use polymorphism to create an object of type IQuittable and call the Quite() method
+            
+            Manager Man1 = new Manager() { firstName = "Big", lastName = "Wig" };
+            Man1.SayName();
+            Man1.Quit();
+
+            Console.ReadLine();
+        }
+        
+    }
+}
