cd02538db4648028419b1caef20efa59b8a21ab0
Robert Ryan <coachrobertryan@gmail.com>
2019-11-27 11:03:40 -0800

    C-Sharp Step 137 Create Struct

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 137 Create Struct/C-Sharp Step 137 Create Struct/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_137_Create_Struct
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number total;
+            total.Amount = 4.75m;
+            Console.WriteLine(total.Amount);
+
+            Console.ReadLine();
+        }
+    }
+}
