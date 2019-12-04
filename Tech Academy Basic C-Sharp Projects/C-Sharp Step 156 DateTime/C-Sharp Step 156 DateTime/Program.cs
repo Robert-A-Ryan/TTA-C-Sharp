da1dee08071bb21f1f5f19ef336f91e07033f05c
Robert Ryan <coachrobertryan@gmail.com>
2019-12-04 10:19:11 -0800

    C-Sharp Step 156 DateTime init

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 156 DateTime/C-Sharp Step 156 DateTime/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_156_DateTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("\n\nPlease enter a number.");
+
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            DateTime dateTime = new DateTime();
+            dateTime = DateTime.Now.AddHours(userNumber);
+            Console.WriteLine("\n\nIt will be {0} in {1} hours.", dateTime, userNumber);
+
+            Console.ReadLine();
+        }
+    }
+}
