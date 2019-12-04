54692669624f6915c0a3b688669964ba70bb0f0b
Robert Ryan <coachrobertryan@gmail.com>
2019-12-04 12:50:41 -0800

    C-Sharp Step 162 Const Var init

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 162 const-var/C-Sharp Step 162 const-var/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_162_const_var
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string homeTown = "Renton, WA";
+
+            var dadHomeTown = "Ada, OK";
+
+            Console.WriteLine("My Dad is from {0}, and I am from {1}.", dadHomeTown, homeTown);
+
+            Console.ReadLine();
+        }
+    }
+}
