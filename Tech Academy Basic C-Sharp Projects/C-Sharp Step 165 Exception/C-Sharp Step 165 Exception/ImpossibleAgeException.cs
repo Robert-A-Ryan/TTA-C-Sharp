2e8a08396892cc1adcb8e9af2fa418307f2448ad
Robert Ryan <coachrobertryan@gmail.com>
2019-12-04 14:12:15 -0800

    C-Sharp Step 165 Exception init

---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 165 Exception/C-Sharp Step 165 Exception/ImpossibleAgeException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_165_Exception
+{
+    public class ImpossibleAgeException : Exception
+    {
+        public ImpossibleAgeException()
+            : base() { }
+        public ImpossibleAgeException(string message)
+            : base(message) { }
+    }
+}
---
+++ Tech Academy Basic C-Sharp Projects/C-Sharp Step 165 Exception/C-Sharp Step 165 Exception/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Step_165_Exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+
+            try
+            {
+                int uAge = Convert.ToInt32(Console.ReadLine());
+                if (uAge < 1)
+                {
+                    throw new ImpossibleAgeException();
+                }
+                var uYear = DateTime.Now.AddYears(-uAge);
+                Console.WriteLine("You were born in {0}.", uYear.Year);
+                Console.ReadLine();
+            }
+            catch (ImpossibleAgeException)
+            {
+                Console.WriteLine("LOL, you can't be 0 or less years old.");
+                Console.ReadLine();
+                return;
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("An error occurred, please contact your Systems Administrator.");
+                Console.ReadLine();
+                return; 
+            }
+
+        }
+    }
+}
