69ca486c2241bbbf323fd79e6242fc1cad41fc35
Robert Ryan <coachrobertryan@gmail.com>
2019-12-09 12:58:38 -0800

    C-Sharp Newsletter MVC App Init

---
+++ Tech Academy C-Sharp Coding Projects/NewsletterAppMVC/Controllers/NewsletterEntities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NewsletterAppMVC.Controllers
+{
+    internal class NewsletterEntities
+    {
+        public object SignUps { get; internal set; }
+
+        internal void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
